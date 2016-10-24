af0c6c29a85fa31b8ba60f30050ed695fc38b6f4
Bima Setiaji <webheadmail@gmail.com>
2016-10-24 15:14:39 +0700

    update with api key

--- js/my-app.js
+++ js/my-app.js
@@ -1,7 +1,7 @@
 // Initialize your app
 var myApp = new Framework7({
     material : true,
-    hideNavbarOnPageScroll : true,
+    //hideNavbarOnPageScroll : true,
     swipePanel: 'left',
     cache:true,
     precompileTemplates: true
@@ -21,255 +21,121 @@ var base_url = 'http://produksi.jmc.co.id/2016/dataku_diy';
 var mainView = myApp.addView('.view-main', {
 });
 
-myApp.onPageInit('home', function (page) {
-    url = base_url + '/api/home';
-
-    $$.getJSON(url, {}, function (data) {          
-        var context = data;
-
-        var template = $$('#home_template').html();
-        var compiledTemplate = Template7.compile(template);
-
-        var html = compiledTemplate(context);
-        
-        $$('#home').html(html);
-    });
-
-
-}).trigger();
-
-myApp.onPageInit('publikasi', function (page) {
-    loadPage();
-
-    // Pull to refresh content
-    var ptrContent = $$('.pull-to-refresh-content');
-     
-    // Add 'refresh' listener on it
-    ptrContent.on('refresh', function (e) {
-        // Emulate 2s loading
-        setTimeout(function () {
-            loadPage();
-            myApp.pullToRefreshDone();
-        }, 2000);
-    });
-    function loadPage(){
-        url = base_url + '/api/publikasi/index';
-
-        $$.getJSON(url, {}, function (data) {          
+function AjaxPage(url,id_page){
+    $$.ajax({
+        url: url,
+        type: "post",
+        data: {'api_key' : '1f69c3d19d24780583a95be95d61ad29b417e6dd'},
+        async: true,
+        dataType: "json",
+        success: function(data) {
             var context = data;
 
-            var template = $$('#publikasi_template').html();
+            var template = $$(id_page+'_template').html();
             var compiledTemplate = Template7.compile(template);
 
             var html = compiledTemplate(context);
-            $$('#publikasi').html(html);
-        });
-    }
+            
+            $$(id_page).html(html);
+        },
+        error: function (textStatus, errorThrown) {
+            
+        }
+    });
+}
+
+myApp.onPageInit('publikasi', function (page) {
+    url = base_url + '/api/publikasi/index';
+    AjaxPage(url,'#publikasi');
+
+    var ptrContent = $$('.pull-to-refresh-content');
+    ptrContent.on('refresh', function (e) {
+        setTimeout(function () {
+            AjaxPage(url,'#publikasi');
+            myApp.pullToRefreshDone();
+        }, 2000);
+    });
 
 });
 myApp.onPageInit('publikasi_detail', function (page) {
         url = base_url + '/api/publikasi/detail/'+ page.query.id;
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#publikasi_detail_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#publikasi_detail').html(html);
-        });
+        AjaxPage(url,'#publikasi_detail');
 });
 
 myApp.onPageInit('indikator_kinerja_utama', function (page) {
         url = base_url + '/api/page/indikator_kinerja_utama';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#indikator_kinerja_utama_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#indikator_kinerja_utama').html(html);
-        });
+        AjaxPage(url,'#indikator_kinerja_utama');
 });
 
 myApp.onPageInit('kinerja_pemda', function (page) {
-    loadPage();
+    url = base_url + '/api/kinerja_pemda';
+    AjaxPage(url,'#kinerja_pemda');
 
-    // Pull to refresh content
     var ptrContent = $$('.pull-to-refresh-content');
-     
-    // Add 'refresh' listener on it
     ptrContent.on('refresh', function (e) {
-        // Emulate 2s loading
         setTimeout(function () {
-            loadPage();
+            AjaxPage(url,'#kinerja_pemda');
             myApp.pullToRefreshDone();
         }, 2000);
     });
-    function loadPage(){
-        url = base_url + '/api/kinerja_pemda';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#kinerja_pemda_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            
-            $$('#kinerja_pemda').html(html);
-        });
-    }
 
 });
 myApp.onPageInit('kinerja_pemda_detail', function (page) {
-        url = base_url + '/api/kinerja_pemda/detail/'+ page.query.id;
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#kinerja_pemda_detail_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#kinerja_pemda_detail').html(html);
-        });
+    url = base_url + '/api/kinerja_pemda/detail/'+ page.query.id;
+    AjaxPage(url,'#kinerja_pemda_detail');
 });
 
 myApp.onPageInit('peta', function (page) {
-    loadPage();
+    url = base_url + '/api/peta';
+    AjaxPage(url,'#peta');
 
-    // Pull to refresh content
     var ptrContent = $$('.pull-to-refresh-content');
-     
-    // Add 'refresh' listener on it
     ptrContent.on('refresh', function (e) {
-        // Emulate 2s loading
         setTimeout(function () {
-            loadPage();
+            AjaxPage(url,'#peta');
             myApp.pullToRefreshDone();
         }, 2000);
     });
-    function loadPage(){
-        url = base_url + '/api/peta';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#peta_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            
-            $$('#peta').html(html);
-        });
-    }
-
 });
 myApp.onPageInit('peta_detail', function (page) {
-        url = base_url + '/api/peta/detail/'+ page.query.id;
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#peta_detail_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#peta_detail').html(html);
-        });
+    url = base_url + '/api/peta/detail/'+ page.query.id;
+    AjaxPage(url,'#peta_detail');
 });
 
 myApp.onPageInit('informasi_statistik', function (page) {
-    loadPage();
+    url = base_url + '/api/informasi_statistik';
+    AjaxPage(url,'#informasi_statistik');
 
-    // Pull to refresh content
     var ptrContent = $$('.pull-to-refresh-content');
-     
-    // Add 'refresh' listener on it
     ptrContent.on('refresh', function (e) {
-        // Emulate 2s loading
         setTimeout(function () {
-            loadPage();
+            AjaxPage(url,'#informasi_statistik');
             myApp.pullToRefreshDone();
         }, 2000);
     });
-    function loadPage(){
-        url = base_url + '/api/informasi_statistik';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#informasi_statistik_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            
-            $$('#informasi_statistik').html(html);
-        });
-    }
 
 });
 myApp.onPageInit('informasi_statistik_detail', function (page) {
-        url = base_url + '/api/informasi_statistik/detail/'+ page.query.id;
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#informasi_statistik_detail_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#informasi_statistik_detail').html(html);
-        });
+    url = base_url + '/api/informasi_statistik/detail/'+ page.query.id;
+    AjaxPage(url,'#informasi_statistik_detail');
 });
 
 myApp.onPageInit('video', function (page) {
-    loadPage();
+    url = base_url + '/api/video';
+    AjaxPage(url,'#video');
 
-    // Pull to refresh content
     var ptrContent = $$('.pull-to-refresh-content');
-     
-    // Add 'refresh' listener on it
     ptrContent.on('refresh', function (e) {
-        // Emulate 2s loading
         setTimeout(function () {
-            loadPage();
+            AjaxPage(url,'#video');
             myApp.pullToRefreshDone();
         }, 2000);
     });
-    function loadPage(){
-        url = base_url + '/api/video';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#video_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            
-            $$('#video').html(html);
-        });
-    }
-
 });
 
 myApp.onPageInit('about', function (page) {
-        url = base_url + '/api/page/tentang_kami';
-
-        $$.getJSON(url, {}, function (data) {          
-            var context = data;
-
-            var template = $$('#about_template').html();
-            var compiledTemplate = Template7.compile(template);
-
-            var html = compiledTemplate(context);
-            $$('#about').html(html);
-        });
+    url = base_url + '/api/page/tentang_kami';
+    AjaxPage(url,'#about');
 });
 
 function youtubePlay(id){
